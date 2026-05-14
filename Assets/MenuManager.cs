f544ada1ca9426157ed49525823950329018d3cd
yorne <aquaalphaazer@gmail.com>
2026-05-14 12:34:19 +0200

    Menu: load SampleScene from PlayGame

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -6,7 +6,7 @@ public class MenuManager : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void OpenOptions()
